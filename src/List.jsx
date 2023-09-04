ac4074aabf28e669a645e1615853cffedca324ad
AvailablA <manohar.shewale77@gmail.com>
2023-09-04 11:59:20 +0530

    Demo5 react file adding

---
+++ src/List.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+function List()
+{
+    return (
+        <>
+            <ol>
+                <li>Manali</li>
+                <li>Saputara</li>
+                <li>Golden Temple</li>
+                <li>Lonavala</li>
+                <li>Shirdi</li>
+            </ol>
+        </>
+    );
+}
+
+export default List;
